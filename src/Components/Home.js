8096c778c7aecfc0a1ef22ab1d3ec693b422d48a
Alex Sharnin <alexsharnin@yandex.ru>
2018-10-26 20:45:41 +0300

    server qql works

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,32 +1,75 @@
 import React, { Component } from 'react'
 import {Query} from 'apollo-client'
 import ggl from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-const test = ggl`
-  {
-    posts {
-      id
-      title
-      body
-    }
-  }
-`
-
-class Home extends Component {
-  render(){
-		return(
-			<div>
-				<h1>It's HOME</h1>
-				{/* <Query query={test}>
-					{({loading, data}) => {
-						if(loading) return 'Loading...'
-						const { posts } = data
-						return posts.map((post) => <h1>{post.title}</h1>)
-					}}
-				</Query> */}
-			</div>
-		)
-	}
+const Home = ({data}) => {
+	if(data.loading) return 'Loading ...'
+	const {posts} = data
+	return(
+		<div>
+			{posts.map((post, index) => <h1 key={index}>{post.title}</h1>)}
+		</div>
+	)
 }
 
-export default Home
+export default graphql(ggl`
+{
+ posts {
+		id
+		title
+		body
+	}
+}
+`)(Home);
+
+
+
+// const test = ggl`
+//   {
+//     posts {
+//       id
+//       title
+//       body
+//     }
+//   }
+// `
+// export default () => (
+// 	<Query query={test} ssr={false}>
+// 		{({data}) => {
+// 			const {posts} = data
+// 			{posts.map((post) => <h1>{post.title}</h1>)}
+// 		}}
+// 	</Query>
+// )
+
+// class Home extends Component {
+// 	constructor(props) {
+//     super(props)
+//     this.state = {}
+// 	}
+
+//   render(){
+
+// 		const {loading, posts} = this.props
+
+// 		if (loading) {
+//       return <div>Loading...</div>
+//     }
+
+// 		return(
+// 			<div>
+// 				<h1>It's HOME</h1>
+// 				{posts.map((post) => <h1>{post.title}</h1>)}
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default (
+// 	<Query query={test} ssr={false}>
+// 		{({loading, data}) => (
+// 			<Home posts={data.posts} loading={loading} />
+// 		)}
+// 	</Query>
+// )
